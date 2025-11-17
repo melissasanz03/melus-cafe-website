28723938d7878e8d842f7f21fb5c1d1f02aac931
Melissa Sanchez <melissa.sanchezpena@stonybrook.edu>
2025-11-17 17:36:54 -0500

    Fix menu images mapping + update MenuDisplay

--- src/components/MenuDisplay.js
+++ src/components/MenuDisplay.js
@@ -20,31 +20,7 @@ export default function MenuDisplay(){
 
               ))}
 
-                {/*<div className="menu-item">
-                    <div className="menu-thumb">
-                      <img src="images/pastry-1.jpg" alt="Matcha latte" className="menu-image"/>
-                    </div>
-                    <p className="menu-label">Matcha latte</p>
-                  </div>
 
-                  <div className="menu-item">
-                    <div className="menu-thumb">
-                      <img src="images/pastry-1.jpg" alt="Matcha latte" className="menu-image"/>
-                    </div>
-                    <p className="menu-label">Matcha latte</p>
-                  </div>
-                  <div className="menu-item">
-                    <div className="menu-thumb">
-                      <img src="images/pastry-1.jpg" alt="Matcha latte" className="menu-image"/>
-                    </div>
-                    <p className="menu-label">Matcha latte</p>
-                  </div>
-                  <div className="menu-item">
-                    <div className="menu-thumb">
-                      <img src="images/pastry-1.jpg" alt="Matcha latte" className="menu-image"/>
-                    </div>
-                    <p className="menu-label">Matcha latte</p>
-                  </div> */}
             </div>
 
             <div className="menu-cta">
